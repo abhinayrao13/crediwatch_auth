317f6e80623cfa642f0f15e70d91177c874e8e2a
Abhinay Rao <abhinayrao13@gmail.com>
2020-06-06 01:30:00 +0530

    Invalid authenticity token hot fix

--- app/controllers/crediwatch_auth/application_controller.rb
+++ app/controllers/crediwatch_auth/application_controller.rb
@@ -1,5 +1,5 @@
 module CrediwatchAuth
   class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+    protect_from_forgery with: :null_session, prepend: true
   end
 end
